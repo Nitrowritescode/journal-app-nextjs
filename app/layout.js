3da8c9d1890f67bd1d5aaabc17332ff6495772ed
StoriesOnTips <storiesontips@gmail.com>
2025-09-03 04:15:40 +0530

    first commit

--- app/layout.js
+++ app/layout.js
@@ -15,23 +15,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider
-    // appearance={{
-      // baseTheme: shadesOfPurple,
-      // variables: {
-      //   colorPrimary: "#3b82f6",
-      //   colorBackground: "#1a202c",
-      //   colorInputBackground: "#2D3748",
-      //   colorInputText: "#F3F4F6",
-      // },
-      // elements: {
-      //   formButtonPrimary: "bg-purple-600 hover:bg-purple-700 text-white",
-      //   card: "bg-gray-800",
-      //   headerTitle: "text-blue-400",
-      //   headerSubtitle: "text-gray-400",
-    //   },
-    // }}
-    >
+    <ClerkProvider>
       <html lang="en">
         <body
           className={`${inter.className} bg-gradient-to-b from-orange-50 via-amber-50 to-orange-50`}
